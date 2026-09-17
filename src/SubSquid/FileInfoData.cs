71a8010e171424f042462c9adfe8dc30f7b2ae8c
nkc-nakanishi <nkc25.nakanishi.tatsumi@gmail.com>
2026-09-17 12:19:48 +0900

    Add files via upload

---
+++ src/SubSquid/FileInfoData.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SubSquid
+{
+  class FileInfoData 
+    {
+        public string FileName;
+        public int Lines;
+        public long SizeKB;
+        public string Updated;
+        public string FullPath;
+    }
+
+}
